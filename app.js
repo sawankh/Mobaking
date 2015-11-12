f80c172955848b910b03978fd825e905a205eb64
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-11-12 14:22:50 +0000

    Cleaned project ready to use

--- app.js
+++ app.js
@@ -14,31 +14,7 @@ Database and Models
 */
 mongoose.connect("mongodb://localhost/myapp");
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    title: String,
-    name: String,
-    surname: String,
-    age: Number,
-    sex: String,
-    idType: String,
-    idNumber: String,
-    address: String,
-    locality: String,
-    city: String,
-    zipCode: Number,
-    country: String,
-    email: String,
-    phone: Number,
-    secondPhone: Number,
-    heartPatient: String,
-    respiratoryPatient: String,
-    Hemophilia: String,
-    bloodGroup: String,
-    allergies: String,
-    otherComments: String,
-    salt: String,
-    hash: String
+    
 });
 
 var User = mongoose.model('users', UserSchema);
@@ -64,171 +40,24 @@ app.use(function (req, res, next) {
     if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
     next();
 });
+
+
 /*
 Helper Functions
 */
-function authenticate(name, pass, fn) {
-    if (!module.parent) console.log('authenticating %s:%s', name, pass);
 
-    User.findOne({
-        username: name
-    },
 
-    function (err, user) {
-        if (user) {
-            if (err) return fn(new Error('cannot find user'));
-            hash(pass, user.salt, function (err, hash) {
-                if (err) return fn(err);
-                if (hash == user.hash) return fn(null, user);
-                fn(new Error('invalid password'));
-            });
-        } else {
-            return fn(new Error('cannot find user'));
-        }
-    });
-
-}
-
-function requiredAuthentication(req, res, next) {
-    if (req.session.user) {
-        next();
-    } else {
-        req.session.error = 'Access denied!';
-        res.redirect('/login');
-    }
-}
-
-function userExist(req, res, next) {
-    User.count({
-        username: req.body.id_username
-    }, function (err, count) {
-        if (count === 0) {
-            next();
-        } else {
-            req.session.error = "User Exist"
-            res.redirect("/signup");
-        }
-    });
-}
 
 /*
 Routes
 */
 app.get("/", function (req, res) {
 
-    if (req.session.user) {
-        res.send("Welcome " + req.session.user.username + "<br>" + "<a href='/logout'>logout</a>");
-    } else {
-        res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
-        //res.render("login");
-    }
+
+
 });
 
-app.get("/signup", function (req, res) {
-    if (req.session.user) {
-        res.redirect("/");
-    } else {
-        res.render("signup");
-    }
-});
 
-app.post("/signup", userExist, function (req, res) {
-    var password = req.body.id_password;
-    var username = req.body.id_username;
-    var title = req.body.id_title;
-    var first_name = req.body.id_first_name;
-    var last_name = req.body.id_last_name;
-    var age = req.body.id_age;
-    var sex = req.body.id_sex;
-    var id_type = req.body.id_identification;
-    var id_number = req.body.id_number;
-    var address = req.body.id_address;
-    var locality = req.body.id_locality;
-    var city = req.body.id_city;
-    var zip_code = req.body.id_zip_code;
-    var country = req.body.id_country;
-    var email = req.body.id_email;
-    var phone = req.body.id_phone;
-    var alternative_phone = req.body.id_alternative_phone;
-    var heart = req.body.id_heart;
-    var respiratory = req.body.id_respiratory;
-    var blood = req.body.id_blood;
-    var hemophilia = req.body.id_hemophilia;
-    var allergies = req.body.id_allergies;
-    var comments = req.body.id_comments;
-
-    hash(password, function (err, salt, hash) {
-        if (err) throw err;
-        var user = new User({
-            username: username,
-            title: title,
-            name: first_name,
-            surname: last_name,
-            age: age,
-            sex: sex,
-            idType: id_type,
-            idNumber: id_number,
-            address: address,
-            locality: locality,
-            city: city,
-            zipCode: zip_code,
-            country: country,
-            email: email,
-            phone: phone,
-            secondPhone: alternative_phone,
-            heartPatient: heart,
-            respiratoryPatient: respiratory,
-            Hemophilia: hemophilia,
-            bloodGroup: blood,
-            allergies: allergies,
-            otherComments: comments,
-            salt: salt,
-            hash: hash,
-        }).save(function (err, newUser) {
-            if (err) throw err;
-            authenticate(newUser.username, password, function(err, user){
-                if(user){
-                    req.session.regenerate(function(){
-                        req.session.user = user;
-                        req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
-                        res.redirect('/');
-                    });
-                }
-            });
-        });
-    });
-});
-
-app.get("/login", function (req, res) {
-    res.render("login");
-});
-
-app.post("/login", function (req, res) {
-    authenticate(req.body.username, req.body.password, function (err, user) {
-        if (user) {
-
-            req.session.regenerate(function () {
-
-                req.session.user = user;
-                req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
-                res.redirect('/');
-            });
-        } else {
-            req.session.error = 'Authentication failed, please check your ' + ' username and password.';
-            res.redirect('/login');
-        }
-    });
-});
-
-app.get('/logout', function (req, res) {
-    req.session.destroy(function () {
-        res.redirect('/');
-    });
-});
-
-app.get('/profile', requiredAuthentication, function (req, res) {
-    res.send('Profile page of '+ req.session.user.username +'<br>'+' click to <a href="/logout">logout</a>');
-});
 
 
 http.createServer(app).listen(3000);
