60ba9b69a2fdfdd1d3b4e6aa3a5de5f3ab2e9f8f
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-11-24 17:19:45 +0000

    Dropped the idea of database for the game images better access the resources in a static way

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ Database and Models
 mongoose.connect("mongodb://localhost/myapp");
 
 var gameSchema = new mongoose.Schema({
-    gameName: String,
-    firstImage: String,
-    secondImage: String,
-    thirdImage: String,
-    fourthImage: String,
-    fifthImage: String,
 });
 
 var game = mongoose.model('games', gameSchema);
