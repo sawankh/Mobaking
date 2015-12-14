971a755b9d03337227a3e19d2cf610db78ec9392
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-14 19:49:07 +0000

    back to square one

--- scraper/mobaking_scraper.py
+++ scraper/mobaking_scraper.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 mobaking_scraper.py
-
 Created on Mon Dec 14 15:20:42 2015
-
 @author: Sawan J. Kapai Harpalani
 """
 
@@ -19,10 +17,10 @@ import numpy as np
 from pandas import DataFrame
 import sys
 
-monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-def ScrapeTwoMonths(keyword, year, startMonth):
-    print 'Scraping '+monthList[startMonth-1]+' and '+monthList[startMonth]+' in '+str(year)
+def ScrapeTwoMonths(keyword, year, startmonth):
+    print 'Scraping '+month_list[startmonth-1]+' and '+month_list[startmonth]+' in '+str(year)
     URL_start = "http://www.google.com/trends/trendsReport?&q="
     URL_end = "&cmpt=q&content=1&export=1"
   
@@ -33,23 +31,28 @@ def ScrapeTwoMonths(keyword, year, startMonth):
             queries_list.append(keyword[i])
         queries = '%20'.join(queries_list)
         
-    date = '&date='+str(startMonth)+'%2F'+str(year)+'%202m'
+    date = '&date='+str(startmonth)+'%2F'+str(year)+'%202m'
     
     URL = URL_start+queries+date+URL_end
 
-    webbrowser.open(URL)   
+    webbrowser.open(URL)    
+    
+# ScrapeRange will download all files for the relevant months.
+#
+# WARNING: Before you run ScrapeRange, ensure your /Downloads folder
+# does not contain any .csv files. 
 
-def ScrapeRange(keyword, startMonth, startYear, endMonth, endYear):
+def ScrapeRange(keyword, startmonth, startyear, endmonth, endyear):
             
-    for i in range(startMonth,13,2):
-        ScrapeTwoMonths(keyword,startYear,i)
+    for i in range(startmonth,13,2):
+        ScrapeTwoMonths(keyword,startyear,i)
         time.sleep(7)
-    for y in range(startYear + 1, endYear):
+    for y in range(startyear + 1, endyear):
         for i in range(1,11,2):
             ScrapeTwoMonths(keyword,y,i)
             time.sleep(7)
-    for i in range(1,endMonth,2):
-        ScrapeTwoMonths(keyword,endYear,i)
+    for i in range(1,endmonth,2):
+        ScrapeTwoMonths(keyword,endyear,i)
         time.sleep(7)
     
     files = copy.deepcopy(os.listdir(path))    
@@ -61,29 +64,45 @@ def ScrapeRange(keyword, startMonth, startYear, endMonth, endYear):
                     oldname = path+'/'+files[i]
                     no = oldname.split('(')[1].split(')')[0]
                     newname = path+'/report'+no+'.csv'
-                    print newname
                     os.rename(oldname,newname)
             except OSError:
                 pass
 
-    quaterlyFiles = [fn for fn in os.listdir(path) if fn.lower().startswith('report')]
+    quarterly_files = [fn for fn in os.listdir(path) if fn.lower().startswith('report')]
                                     
-    for file in quaterlyFiles:
-        shutil.move(path+"/"+file,path+'/'+scrapingDir)
+    for file in quarterly_files:
+        shutil.move(path+"/"+file,path+'/'+scrapings_dir)
 
-    fullPath = path+'/'+scrapingDir    
-    newFiles = copy.deepcopy(os.listdir(fullPath))
+    full_path = path+'/'+scrapings_dir    
+    newfiles = copy.deepcopy(os.listdir(full_path))
 
-    for i in range(0,len(newFiles)):
-        oldname = fullPath+'/'+newFiles[i]
+    for i in range(0,len(newfiles)):
+        oldname = full_path+'/'+newfiles[i]
         if os.path.getsize(oldname) < 800:
             print 'File '+oldname+' is unusually small...'
-        newname = fullPath+'/'+str(os.path.getmtime(fullPath+'/'+newFiles[i]))[:-2]+".csv"
-        os.rename(oldname, newname) 
+        newname = full_path+'/'+str(os.path.getmtime(full_path+'/'+newfiles[i]))[:-2]+".csv"
+        os.rename(oldname, newname)       
 
-def ScrapeRangeWeekly(keyword, startMonth, startYear, endMonth, endYear):
-    months = 11-startMonth + (endYear - startYear - 1)*12 + endMonth
-    print 'Scraping weekly data from '+month_list[startMonth-1]+' '+str(startYear)+' to '+month_list[endMonth-1]+' '+str(endYear)
+
+
+### move files from Downloads folder to /gt_keywords
+### rename files (they can be distinguished by date modified)
+
+### if a quarter only returns weekly data, pad out the weeks by steps:
+
+    ## check weekly or not by filesize
+    ## for weekly files:
+        ## replce week ranges by week start dates
+        ## insert six rows between each row, pad with intervening dates and copy values down
+        ## overwrite files
+
+### copy all these into a single daily list for the full date range
+
+# download weekly data for full period
+
+def ScrapeRangeWeekly(keyword, startmonth, startyear, endmonth, endyear):
+    months = 11-startmonth + (endyear - startyear - 1)*12 + endmonth
+    print 'Scraping weekly data from '+month_list[startmonth-1]+' '+str(startyear)+' to '+month_list[endmonth-1]+' '+str(endyear)
     
     URL_start = "http://www.google.com/trends/trendsReport?&q="
     URL_end = "&cmpt=q&content=1&export=1"
@@ -95,7 +114,7 @@ def ScrapeRangeWeekly(keyword, startMonth, startYear, endMonth, endYear):
             queries_list.append(keyword[i])
         queries = '%20'.join(queries_list)
 
-    date = '&date='+str(startMonth)+'%2F'+str(startYear)+'%20'+str(months)+'m'
+    date = '&date='+str(startmonth)+'%2F'+str(startyear)+'%20'+str(months)+'m'
     
     URL = URL_start+queries+date+URL_end
     
@@ -204,24 +223,25 @@ def StitchFrames():
 
     final_data_frame.to_csv(final_name, sep=',')
 
-## run as python mobaking_scraper.py startMonth startYear endMonth endYear keyword_list
+## run as python gt_scraper.py startmonth startyear endmonth endyear keyword_list
 
 if __name__ == '__main__':
 
     print sys.argv[0]
 
-    startMonth = sys.argv[1]
-    startYear = sys.argv[2]
-    endMonth = sys.argv[3]
-    endYear = sys.argv[4]
+    startmonth = sys.argv[1]
+    startyear = sys.argv[2]
+    endmonth = sys.argv[3]
+    endyear = sys.argv[4]
     keywords = sys.argv[5:]
 
-    path = '../data'
+    path = '/Users/clintonboys/Downloads'
 
-    scrapingsDir = 'keyword_{0}'.format(keywords[0])
+    scrapings_dir = 'gt_{0}'.format(keywords[0])
     if not os.path.exists(path+"/"+scrapings_dir):
         os.makedirs(path+"/"+scrapings_dir)
 
-    ScrapeRange(keywords, int(startMonth), int(startYear), int(endMonth), int(endYear))
-    
+    ScrapeRange(keywords, int(startmonth), int(startyear), int(endmonth), int(endyear))
+    ScrapeRangeWeekly(keywords, int(startmonth), int(startyear), int(endmonth), int(endyear))
+
     StitchFrames()
