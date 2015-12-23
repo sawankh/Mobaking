b3105b0b1eae5647fd0c8f2759a207ed29f7cc06
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-23 17:40:38 +0000

    Applied schema of routing to other games

--- app.js
+++ app.js
@@ -57,6 +57,17 @@ app.get("/", function (req, res) {
 
 });
 
+app.get("/world", function (req, res) {
+
+    res.render("world");
+
+});
+
+app.get("/about", function (req, res) {
+
+    res.render("about");
+
+});
 
 //----------------------------- LOL ----------------------------- //
 app.get("/lol", function (req, res) {
@@ -117,11 +128,90 @@ app.get("/dota2", function (req, res) {
 
 });
 
+app.get("/dota2/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "dota2",
+    });
+
+});
+
+app.get("/dota2/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "dota2",
+    });
+
+});
+
+app.get("/dota2/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "dota2",
+    });
+
+});
+
+app.get("/dota2/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "dota2",
+    });
+
+});
+
+app.get("/dota2/future", function (req, res) {
+
+    res.render("future", {
+        currentGame: "dota2",
+    });
+
+});
 
 //----------------------------- AIRMECH ----------------------------- //
 app.get("/airmech", function (req, res) {
 
     res.render("game", {
+        currentGame: "airmech",
+    });
+
+});
+
+app.get("/airmech/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "airmech",
+    });
+
+});
+
+app.get("/airmech/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "airmech",
+    });
+
+});
+
+app.get("/airmech/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "airmech",
+    });
+
+});
+
+app.get("/airmech/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "airmech",
+    });
+
+});
+
+app.get("/airmech/future", function (req, res) {
+
+    res.render("future", {
         currentGame: "airmech",
     });
 
@@ -136,10 +226,90 @@ app.get("/hos", function (req, res) {
 
 });
 
+app.get("/hos/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "hos",
+    });
+
+});
+
+app.get("/hos/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "hos",
+    });
+
+});
+
+app.get("/hos/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "hos",
+    });
+
+});
+
+app.get("/hos/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "hos",
+    });
+
+});
+
+app.get("/hos/future", function (req, res) {
+
+    res.render("future", {
+        currentGame: "hos",
+    });
+
+});
+
 //----------------------------- IC ----------------------------- //
 app.get("/ic", function (req, res) {
 
     res.render("game", {
+        currentGame: "ic",
+    });
+
+});
+
+app.get("/ic/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "ic",
+    });
+
+});
+
+app.get("/ic/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "ic",
+    });
+
+});
+
+app.get("/ic/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "ic",
+    });
+
+});
+
+app.get("/ic/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "ic",
+    });
+
+});
+
+app.get("/ic/future", function (req, res) {
+
+    res.render("future", {
         currentGame: "ic",
     });
 
@@ -154,10 +324,90 @@ app.get("/smite", function (req, res) {
 
 });
 
+app.get("/smite/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "smite",
+    });
+
+});
+
+app.get("/smite/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "smite",
+    });
+
+});
+
+app.get("/smite/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "smite",
+    });
+
+});
+
+app.get("/smite/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "smite",
+    });
+
+});
+
+app.get("/smite/future", function (req, res) {
+
+    res.render("future", {
+        currentGame: "smite",
+    });
+
+});
+
 //----------------------------- OVERWATCH ----------------------------- //
 app.get("/overwatch", function (req, res) {
 
     res.render("game", {
+        currentGame: "overwatch",
+    });
+
+});
+
+app.get("/overwatch/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "overwatch",
+    });
+
+});
+
+app.get("/overwatch/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "overwatch",
+    });
+
+});
+
+app.get("/overwatch/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "overwatch",
+    });
+
+});
+
+app.get("/overwatch/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "overwatch",
+    });
+
+});
+
+app.get("/overwatch/future", function (req, res) {
+
+    res.render("future", {
         currentGame: "overwatch",
     });
 
@@ -172,10 +422,90 @@ app.get("/strife", function (req, res) {
 
 });
 
+app.get("/strife/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "strife",
+    });
+
+});
+
+app.get("/strife/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "strife",
+    });
+
+});
+
+app.get("/strife/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "strife",
+    });
+
+});
+
+app.get("/strife/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "strife",
+    });
+
+});
+
+app.get("/strife/future", function (req, res) {
+
+    res.render("future", {
+        currentGame: "strife",
+    });
+
+});
+
 //----------------------------- HON ----------------------------- //
 app.get("/hon", function (req, res) {
 
     res.render("game", {
+        currentGame: "hon",
+    });
+
+});
+
+app.get("/hon/intro", function (req, res) {
+
+    res.render("intro", {
+        currentGame: "hon",
+    });
+
+});
+
+app.get("/hon/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "hon",
+    });
+
+});
+
+app.get("/hon/2015", function (req, res) {
+
+    res.render("date_15", {
+        currentGame: "hon",
+    });
+
+});
+
+app.get("/hon/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "hon",
+    });
+
+});
+
+app.get("/hon/future", function (req, res) {
+
+    res.render("future", {
         currentGame: "hon",
     });
 
@@ -190,16 +520,45 @@ app.get("/mww", function (req, res) {
 
 });
 
-app.get("/world", function (req, res) {
+app.get("/mww/intro", function (req, res) {
 
-    res.render("world");
+    res.render("intro", {
+        currentGame: "mww",
+    });
+
+});
+
+app.get("/mww/2014", function (req, res) {
+
+    res.render("date_14", {
+        currentGame: "mww",
+    });
 
 });
 
-app.get("/about", function (req, res) {
+app.get("/mww/2015", function (req, res) {
 
-    res.render("about");
+    res.render("date_15", {
+        currentGame: "mww",
+    });
 
 });
+
+app.get("/mww/world", function (req, res) {
+
+    res.render("game_world", {
+        currentGame: "mww",
+    });
+
+});
+
+app.get("/mww/future", function (req, res) {
+
+    res.render("future", {
+        currentGame: "mww",
+    });
+
+});
+
 
 http.createServer(app).listen(3000);
