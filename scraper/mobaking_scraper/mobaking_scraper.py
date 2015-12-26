4311f8fd7356bb242a26f20265be6a610b596bd4
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-26 16:44:31 +0000

    Fixed bug directories

--- scraper/mobaking_scraper/mobaking_scraper.py
+++ scraper/mobaking_scraper/mobaking_scraper.py
@@ -3,28 +3,25 @@ import time
 import os, sys
 from random import randint
 
-google_username = ''
-google_password = ''
-path = ""
+google_username = 'pytrends@gmail.com'
+google_password = 'datascience'
+path = "/Users/Sawan/Desktop/pytrends-master/examples/"
 
 # connect to Google
 connector = pyGTrends(google_username, google_password)
 
-# Keyword files
+# make request
 files = ["lol", "dota 2", "hon", "hos", "ic", "mww", "overwatch", "smite", "strife"]
 
-# Loop through the files
 for fi in files:
-	# Create folder of the game
-	os.makedirs(path+fi)
-	# Read the file with the keywords
+	if not os.path.exists(path+fi):
+		os.makedirs(path+fi)
 	with open(fi+".txt") as f:
 		for line in f:
-			# Remove the jump line
 			keyword = line.rstrip('\n')
 			year = 2014
-			# Create folder of the keyword
-			os.makedirs(path+fi+"/"+keyword)
+			if not os.path.exists(path+fi+"/"+keyword):
+				os.makedirs(path+fi+"/"+keyword)
 			for y in range(1,3):
 				month = 1
 				for x in range(1,7):
@@ -33,6 +30,5 @@ for fi in files:
 					time.sleep(randint(5, 10))
 					# download file
 					connector.save_csv(path+fi+"/"+keyword+"/", keyword+str(year)+str(x))
-					# Group of two months
 					month = month + 2
 				year = year + 1
