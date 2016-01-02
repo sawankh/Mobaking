e5443dec808d1c44cea204525299285c003aaa8f
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-02 23:56:44 +0000

    Added random colors

--- public/js/scatterChart.js
+++ public/js/scatterChart.js
@@ -1,5 +1,8 @@
 var jq=jQuery.noConflict();
 jq(function () {
+    var colors = ["#D4D4D", "#5DA5DA", "#FAA43A", "#60BD68", "#F17CB0", "#B2912F", "#B276B2","#DECF3F", "#F15854"];
+    var randomColor = parseInt((Math.random() * ((colors.length - 1) - 0 + 1)), 10) + 0;
+    
     jq('#container-scatter').highcharts({
         xAxis: {
             min: -0.5,
