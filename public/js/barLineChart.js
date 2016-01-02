8886d9b0847e9a119bb2a3be0e114a496e34c036
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-02 20:52:31 +0000

    Testing bar line chart

---
+++ public/js/barLineChart.js
@@ -0,0 +1,33 @@
+var jqu=jQuery.noConflict();
+jqu(function () {
+    jqu('#container-area').highcharts({
+        title: {
+            text: 'Combination chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+        },
+        series: [{
+            type: 'column',
+            name: 'Jane',
+            data: [3, 2, 1, 3, 4]
+        }, {
+            type: 'column',
+            name: 'John',
+            data: [2, 3, 5, 7, 6]
+        }, {
+            type: 'column',
+            name: 'Joe',
+            data: [4, 3, 3, 9, 0]
+        }, {
+            type: 'spline',
+            name: 'Average',
+            data: [3, 2.67, 3, 6.33, 3.33],
+            marker: {
+                lineWidth: 2,
+                lineColor: Highcharts.getOptions().colors[3],
+                fillColor: 'white'
+            }
+        }]
+    });
+});
