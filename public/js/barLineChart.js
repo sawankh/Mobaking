a804b91b61d3a56fa9d9e947057be17ccb9f3405
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-02 20:54:39 +0000

    Customizing line chart

--- public/js/barLineChart.js
+++ public/js/barLineChart.js
@@ -12,14 +12,6 @@ jqu(function () {
             name: 'Jane',
             data: [3, 2, 1, 3, 4]
         }, {
-            type: 'column',
-            name: 'John',
-            data: [2, 3, 5, 7, 6]
-        }, {
-            type: 'column',
-            name: 'Joe',
-            data: [4, 3, 3, 9, 0]
-        }, {
             type: 'spline',
             name: 'Average',
             data: [3, 2.67, 3, 6.33, 3.33],
