b303c659657a42372c4c88de04567230c700a3a7
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-03 21:40:04 +0000

    Button works

--- public/js/pick.js
+++ public/js/pick.js
@@ -1,7 +1,10 @@
 var jaq=jQuery.noConflict();
 jaq(function () {
-	jaq('ul').find('li').css('list-style-type', 'none');
 	jaq("select.image-picker").imagepicker({
-		hide_select:  false,
+		hide_select:  true,
 	});
+});
+
+jaq("#compare").click(function () {
+	alert("Hello!");
 });
