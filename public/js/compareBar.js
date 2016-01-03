e7518fc4220d51a47de6eab686d9739e2a795081
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-03 21:55:55 +0000

    Bar chart

--- public/js/compareBar.js
+++ public/js/compareBar.js
@@ -1,56 +1,70 @@
 var jqu=jQuery.noConflict();
 jqu("#compare").click(function () {
+	var categories = ['0-4', '5-9', '10-14', '15-19',
+	        '20-24', '25-29', '30-34', '35-39', '40-44',
+	        '45-49', '50-54', '55-59', '60-64', '65-69',
+	        '70-74', '75-79', '80-84', '85-89', '90-94',
+	        '95-99', '100 + '];
+	
+	    jqu('#container-area').highcharts({
+	        chart: {
+	            type: 'bar'
+	        },
+	        title: {
+	            text: 'Population pyramid for Germany, 2015'
+	        },
+	        subtitle: {
+	            text: 'Source: <a href="http://populationpyramid.net/germany/2015/">Population Pyramids of the World from 1950 to 2100</a>'
+	        },
+	        xAxis: [{
+	            categories: categories,
+	            reversed: false,
+	            labels: {
+	                step: 1
+	            }
+	        }, { // mirror axis on right side
+	            opposite: true,
+	            reversed: false,
+	            categories: categories,
+	            linkedTo: 0,
+	            labels: {
+	                step: 1
+	            }
+	        }],
+	        yAxis: {
+	            title: {
+	                text: null
+	            },
+	            labels: {
+	                formatter: function () {
+	                    return Math.abs(this.value) + '%';
+	                }
+	            }
+	        },
 
-	jqu('#container-area').highcharts({
+	        plotOptions: {
+	            series: {
+	                stacking: 'normal'
+	            }
+	        },
 
-	    chart: {
-	        polar: true,
-	        type: 'line'
-	    },
+	        tooltip: {
+	            formatter: function () {
+	                return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' +
+	                    'Population: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+	            }
+	        },
 
-	    title: {
-	        text: 'Budget vs spending',
-	        x: -80
-	    },
-
-	    pane: {
-	        size: '80%'
-	    },
-
-	    xAxis: {
-	        categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
-	                'Information Technology', 'Administration'],
-	        tickmarkPlacement: 'on',
-	        lineWidth: 0
-	    },
-
-	    yAxis: {
-	        gridLineInterpolation: 'polygon',
-	        lineWidth: 0,
-	        min: 0
-	    },
-
-	    tooltip: {
-	        shared: true,
-	        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
-	    },
-
-	    legend: {
-	        align: 'right',
-	        verticalAlign: 'top',
-	        y: 70,
-	        layout: 'vertical'
-	    },
-
-	    series: [{
-	        name: 'Allocated Budget',
-	        data: [43000, 19000, 60000, 35000, 17000, 10000],
-	        pointPlacement: 'on'
-	    }, {
-	        name: 'Actual Spending',
-	        data: [50000, 39000, 42000, 31000, 26000, 14000],
-	        pointPlacement: 'on'
-	    }]
-
-	});
+	        series: [{
+	            name: 'Male',
+	            data: [-2.2, -2.2, -2.3, -2.5, -2.7, -3.1, -3.2,
+	                -3.0, -3.2, -4.3, -4.4, -3.6, -3.1, -2.4,
+	                -2.5, -2.3, -1.2, -0.6, -0.2, -0.0, -0.0]
+	        }, {
+	            name: 'Female',
+	            data: [2.1, 2.0, 2.2, 2.4, 2.6, 3.0, 3.1, 2.9,
+	                3.1, 4.1, 4.3, 3.6, 3.4, 2.6, 2.9, 2.9,
+	                1.8, 1.2, 0.6, 0.1, 0.0]
+	        }]
+	    });
 });
