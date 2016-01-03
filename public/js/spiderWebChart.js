bc26477919e7a4aba8e02a45991a8ebd3d86044f
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-01-03 19:10:13 +0000

    Testing spiderweb

--- public/js/spiderWebChart.js
+++ public/js/spiderWebChart.js
@@ -14,6 +14,80 @@ jqu(function () {
         axisX_labels.push(label);
     }
 
+    var serieLol = [];
+    var serieDota2 = [];
+    var serieAirmech = [];
+    var serieHos = [];
+    var serieHon = [];
+    var serieMww = [];
+    var serieIc = [];
+    var serieSmite = [];
+    var serieStrife = [];
+    var serieOverwatch = [];
+
+    for (var i = 0; i < world_data.length; i++) {
+        if (world_data[i].game == "lol") {
+            serieLol.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "dota2") {
+            serieDota2.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "airmech") {
+            serieAirmech.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "hos") {
+            serieHos.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "hon") {
+            serieHon.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "ic") {
+            serieIc.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "smite") {
+            serieSmite.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "overwatch") {
+            serieOverwatch.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "mww") {
+            serieMww.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        } else if (world_data[i].game == "strife") {
+            serieStrife.push([world_data[i].value, m.indexOf(world_data[i].period+" "+world_data[i].year)]);
+        };
+    };
+
+    serieLol.sort(compareSecondColumn);
+    serieDota2.sort(compareSecondColumn);
+    serieAirmech.sort(compareSecondColumn);
+    serieHos.sort(compareSecondColumn);
+    serieHon.sort(compareSecondColumn);
+    serieIc.sort(compareSecondColumn);
+    serieSmite.sort(compareSecondColumn);
+    serieStrife.sort(compareSecondColumn);
+    serieOverwatch.sort(compareSecondColumn);
+    serieMww.sort(compareSecondColumn);
+
+    function compareSecondColumn(a, b) {
+        if (a[1] === b[1]) {
+            return 0;
+        }
+        else {
+            return (a[1] < b[1]) ? -1 : 1;
+        }
+    }
+
+    function slice(arr) {
+        var sl = [];
+        for (var i = 0; i < arr.length; i++) {
+            sl.push(arr[i][0]);
+        };
+        return sl;
+    }
+
+    serieLol = slice(serieLol);
+    serieDota2 = slice(serieDota2);
+    serieAirmech = slice(serieAirmech);
+    serieHos = slice(serieHos);
+    serieHon = slice(serieHon);
+    serieIc = slice(serieIc);
+    serieSmite = slice(serieSmite);
+    serieStrife = slice(serieStrife);
+    serieOverwatch = slice(serieOverwatch);
+    serieMww = slice(serieMww);
+
     jqu('#container-radar').highcharts({
 
         chart: {
@@ -54,15 +128,37 @@ jqu(function () {
             layout: 'vertical'
         },
 
-        series: [{
-            name: 'Allocated Budget',
-            data: [43000, 19000, 60000, 35000, 17000, 10000, 43000, 19000, 60000, 35000, 17000, 10000],
-            pointPlacement: 'on'
+        series:[{
+            name: 'League of Legends',
+            data: serieLol
+        }, 
+        {
+            name: 'Dota 2',
+            data: serieDota2
         }, {
-            name: 'Actual Spending',
-            data: [50000, 39000, 42000, 31000, 26000, 14000, 50000, 39000, 42000, 31000, 26000, 14000],
-            pointPlacement: 'on'
+            name: 'Airmech',
+            data: serieAirmech
+        }, {
+            name: 'Heroes of the Storm',
+            data: serieHos
+        }, {
+            name: 'Heroes of Newreth',
+            data: serieHon
+        }, {
+            name: 'Infinite Crisis',
+            data: serieIc
+        }, {
+            name: 'Smite',
+            data: serieSmite
+        }, {
+            name: 'Strife',
+            data: serieStrife
+        }, {
+            name: 'Overwatch',
+            data: serieOverwatch
+        }, {
+            name: 'Magicka Wizard Wars',
+            data: serieMww
         }]
-
     });
 });
